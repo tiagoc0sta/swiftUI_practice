5efd7cd8547b23d4abb1d52815f4716f905866eb
tiagoc0sta <tiagodpv@gmail.com>
2024-08-30 14:19:46 -0400

    add homeView

---
+++ SwiftUinPractice/Spotify/Core/SpotifyHomeView.swift
@@ -0,0 +1,38 @@
+//
+//  SpotifyHomeView.swift
+//  SwiftUinPractice
+//
+//  Created by Tiago de Freitas Costa on 2024-08-30.
+//
+
+import SwiftUI
+
+struct SpotifyHomeView: View {
+    
+    @State private var currentUser: User? = nil
+    
+    var body: some View {
+        ZStack {
+            Color.spotifyBlack.ignoresSafeArea()
+            
+            HStack {
+                if let currentUser {
+                    ImageLoaderView(urlString: currentUser.image)
+                        .frame(width: 30, height: 30)
+                        .background(.spotifyWhite)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                        .onTapGesture {
+                            <#code#>
+                        }
+                }
+            }
+        }
+        .task {
+            <#code#>
+        }
+    }
+}
+
+#Preview {
+    SpotifyHomeView()
+}
